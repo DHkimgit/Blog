e23dab7d0e5d95caeb81ebe9740f213356a8030d
DHKimgit <dukekim001@naver.com>
2024-05-28 14:38:25 +0000

    0528

---
+++ blog-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "eunoia",
+  description: "안녕하세요",
+  author: "eunoia",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/DHkimgit"
+  },
+  utterances: {
+    repo: "DHkimgit/gatsby-starter-hoodie",
+    type: "pathname",
+  },
+}
